6ac65998193ef46d4381a5ee6bcc727ab9d5dae0
ian <odhiamboian096@gmail.com>
2026-08-16 16:39:28 +0300

    another commit

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,15 +1,18 @@
-import express from "express" ;
+import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/v1/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=> {
-    console.log("server is running on http://localhost:" + PORT)
-})
+app.listen(PORT, () => {
+  console.log("server is running on http://localhost:" + PORT);
+});
+
+//my database connection string
+//mongodb+srv://theniloticdecor_db_user:cFu7Vow3ZDozPLCN@cluster0.qvt3x8z.mongodb.net/?appName=Cluster0

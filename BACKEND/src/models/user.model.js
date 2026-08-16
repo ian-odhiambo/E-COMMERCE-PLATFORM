9e75b9734a98e4542a6545d219c3195335e0936c
ian <odhiamboian096@gmail.com>
2026-08-16 18:13:18 +0300

    another commit

---
+++ BACKEND/src/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    pasword:{
+        type: String,
+        required: [true, "password is required"],
+        minlength: [6, "password must be at least 6 characters long"]
+    },
+    cartItems:[
+        {
+            quantity:{
+                type: Number,
+                default: 1
+            },
+            product:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref:"product"
+            }
+        }
+    ],
+    role:{
+        type: String,
+        enum: ["user","admin"],
+        default: "customer"
+    }
+},{
+    timestamps: true,
+}
+);
+
+mongoose.model("User", userSchema)

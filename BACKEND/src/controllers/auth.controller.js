823e25f82c80244ea3984d32074d7db9f5691f7b
ian <odhiamboian096@gmail.com>
2026-08-16 21:22:26 +0300

    another commit

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -2,7 +2,12 @@ import User from "../models/user.model.js";
 
 export const signup = async(req, res) => {
     const { name, email, password } = req.body;
-    res.send("sign up route called");
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+        return res.status(400).json({message: "user already exists"});
+    }
+    const user = await user.create({name, email, password})
 }
 export const login = async(req, res) => {
     res.send("login route called");

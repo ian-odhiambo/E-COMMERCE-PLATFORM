d600e34f22024501eb0511339201c72a705710dc
ian <odhiamboian096@gmail.com>
2026-08-17 17:52:25 +0300

    another commit

--- BACKEND/src/models/user.model.js
+++ BACKEND/src/models/user.model.js
@@ -43,7 +43,10 @@ const userSchema = new mongoose.Schema({
 
 //Pre-save hook to hash password beore saving to database
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+    if (!this.isModified("password")) {
+        next();
+        return:
+    }
 
     try {
         const salt = await bcrypt.genSalt(10);

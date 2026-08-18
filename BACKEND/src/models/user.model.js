5eebdc21c1abc9f2c77f59fd0a8914f27e6eaabb
ian <odhiamboian096@gmail.com>
2026-08-18 13:53:58 +0300

    another commit

--- BACKEND/src/models/user.model.js
+++ BACKEND/src/models/user.model.js
@@ -46,9 +46,9 @@ userSchema.pre("save", function(next) {
     if (!this.isModified("password")) return next();
     
     const user = this;
-    bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.genSalt(10, function(err, salt) {
         if (err) return next(err);
-        bcrypt.hash(user.password, salt, (err, hash) => {
+        bcrypt.hash(user.password, salt, function(err, hash) {
             if (err) return next(err);
             user.password = hash;
             next();

8c1e3e9a1978735152a8477b2028765c6456a993
ian <odhiamboian096@gmail.com>
2026-08-22 13:17:23 +0300

    commit of the day

--- BACKEND/src/libs/redis.js
+++ BACKEND/src/libs/redis.js
@@ -1,7 +1,8 @@
 import { Redis } from '@upstash/redis'
 import dotenv from "dotenv"
 
+
 const redis = new Redis(process.env.UPSTASH_REDIS_REST_URL)
 
-await redis.set("foo", "bar");
-await redis.get("foo");
+// await redis.set("foo", "bar");
+// await redis.get("foo");

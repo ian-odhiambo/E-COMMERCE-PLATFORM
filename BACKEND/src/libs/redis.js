f1c8484205c09e315e59e22642849651a13c23ee
ian <odhiamboian096@gmail.com>
2026-08-22 14:24:11 +0300

    another commit

--- BACKEND/src/libs/redis.js
+++ BACKEND/src/libs/redis.js
@@ -1,13 +1,3 @@
-// import { Redis } from '@upstash/redis'
-// import dotenv from "dotenv"
-
-
-// const redis = new Redis(process.env.UPSTASH_REDIS_REST_URL)
-
-
-// // Redis is like a key-value store, think of it like a json
-// await redis.set("foo", "bar");
-// await redis.get("foo");
 import { Redis } from "@upstash/redis"
 import dotenv from "dotenv"
 dotenv.config()
@@ -19,5 +9,5 @@ const redis = new Redis({
 })
 
 // Example usage
-await redis.set("foo", "bar")
-console.log(await redis.get("foo"))
+// await redis.set("foo", "bar")
+// console.log(await redis.get("foo"))

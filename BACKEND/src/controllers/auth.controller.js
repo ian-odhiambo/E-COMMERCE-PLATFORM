a9bb5dc512055a66245a4906a3b764dde53ece0d
ian <odhiamboian096@gmail.com>
2026-08-23 16:27:17 +0300

    another commit

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -75,6 +75,13 @@ export const login = async(req, res) => {
 
             await storeRefreshToken(user._id,refreshToken)
             setCookies(res,accessToken,refreshToken)
+
+            res.json({
+                _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            })
         }
     }catch(error){
 

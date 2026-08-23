6be333baf098963bbf90c171e431793a88c32ef6
ian <odhiamboian096@gmail.com>
2026-08-23 16:43:42 +0300

    another commit

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -61,6 +61,7 @@ export const signup = async(req, res) => {
             role: user.role,
         })
     } catch(error) {
+        console.log("Error in logout controller", error.message)
         res.status(500).json({ message: error.message})
     }
 };

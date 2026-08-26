0c3c0f8da94ef9125153ddd9c30d067ef8e55feb
ian <odhiamboian096@gmail.com>
2026-08-26 08:44:51 +0300

    another commit

---
+++ BACKEND/src/controllers/product.controller.js
@@ -0,0 +1,11 @@
+import Product from "../model/product.model.js"
+
+export const getAllProducts = async (req, res) => {
+    try{
+        const products = await Product.find({});// This is for find all products
+        res.json({ products });
+    }catch(error){
+        console.log("Error in the gegtAllProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
--- BACKEND/src/routes/v1/product.route.js
+++ BACKEND/src/routes/v1/product.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 
-const router = express.Router
+const router = express.Router();
 
-router.get("/", getAllProducts)
+router.get("/",protectRoute,adminRoute, getAllProducts)
 
 export default router

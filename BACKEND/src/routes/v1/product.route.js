da718eff5fb4b0ba60e9792688ae02e87888fe8e
ian <odhiamboian096@gmail.com>
2026-08-26 09:40:14 +0300

    another commit

--- BACKEND/src/routes/v1/product.route.js
+++ BACKEND/src/routes/v1/product.route.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { getAllProducts } from '../controller/product.controller.js';
 
 const router = express.Router();
 
 router.get("/",protectRoute,adminRoute, getAllProducts)
 
-export default router
+export default router;

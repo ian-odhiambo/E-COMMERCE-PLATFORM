4f903859ceaebca7b8d043576b4919a8713e9bdd
ian <odhiamboian096@gmail.com>
2026-08-26 18:33:24 +0300

    another commit

--- BACKEND/src/routes/v1/product.route.js
+++ BACKEND/src/routes/v1/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from '../../controllers/product.controller.js'; 
+import { getAllProducts, getFeaturedProducts, createProduct } from '../../controllers/product.controller.js'; 
 import { protectRoute, adminRoute } from '../../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/",protectRoute,adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.post("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;

ddf5f690be02e2a7340deeb243bc51c02c3e5d56
ian <odhiamboian096@gmail.com>
2026-08-27 14:24:00 +0300

    another commit

--- BACKEND/src/routes/v1/cart.route.js
+++ BACKEND/src/routes/v1/cart.route.js
@@ -4,6 +4,8 @@ import { protectRoute } from "../../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/",protectRoute, addToCart)
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);// this for updating quantity, either by increasing or decreasing it
 
 export default router

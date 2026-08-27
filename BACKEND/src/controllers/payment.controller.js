3d5d2d93f17e17c1133d7e27e32ff84be3f1dc95
ian <odhiamboian096@gmail.com>
2026-08-27 22:55:01 +0300

    another commit

--- BACKEND/src/controllers/payment.controller.js
+++ BACKEND/src/controllers/payment.controller.js
@@ -57,3 +57,24 @@ export const createCheckoutSession = async (req, res) => {
         }catch(error) {}
     
 }
+
+async function createStripeCoupon(discountPercentage) {
+    const coupon = await stripe.coupons.create({
+        percent_off: discountPercentage,
+        duration: "once",
+    });
+
+    return coupon.id;
+}
+
+async function createNewCoupon(userId){
+    const newCoupon = new Coupon({
+        code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+        discountPercentage:10,
+        expirationDate: new Date(Date.now() + 30 * 24 * 60 * 1000), //30 days from now
+    })
+
+    await newCoupon.save();
+
+    return newCoupon
+}

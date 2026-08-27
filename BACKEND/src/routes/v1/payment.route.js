ee4b74de9f9515f0e5405298da4dbc97d0628050
ian <odhiamboian096@gmail.com>
2026-08-27 23:12:16 +0300

    another commit

--- BACKEND/src/routes/v1/payment.route.js
+++ BACKEND/src/routes/v1/payment.route.js
@@ -7,6 +7,24 @@ const router = express.Router();
 
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, async (req, res) =>{
+    try{
+        const { sessionId } = req.body;
+        const session = await stripe.checkout.session.retrieve(sessionId);
+
+        if(session.payment_status === "paid") {
+            if(session.metadata.couponCode) {
+                await Coupon.findOneAndUpdate({
+                    code: session.metadata.couponCode, userId:session.metadata.userId
+                }, {
+                    isActive:false
+                })
+            }
+        }
+    }catch(error){
+
+    }
+});
    
 
 

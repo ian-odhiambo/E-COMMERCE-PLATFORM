492a02806543e9e8e378fafd9b02689e72143d75
ian <odhiamboian096@gmail.com>
2026-08-27 15:02:24 +0300

    another commit

--- BACKEND/src/controllers/cart.controller.js
+++ BACKEND/src/controllers/cart.controller.js
@@ -47,9 +47,16 @@ export const updateQuantity = async (req, res) => {
                 await user.save();
                 return res.json(user.cartItems);
             }
+
+            existingItem.quantity = quantity;
+            await user.save();
+            res.json(user.cartItems)
+        }else{
+            res.status(404).json({message: "product not found"});
         }
 
     }catch(error){
-
+        console.log("Error in updateQuantity controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
     }
 }

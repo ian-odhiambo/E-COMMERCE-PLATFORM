2a33accb2fba036a07652e24e1d7278f4d291f9b
ian <odhiamboian096@gmail.com>
2026-08-27 21:00:20 +0300

    another commit

--- BACKEND/src/routes/v1/payment.route.js
+++ BACKEND/src/routes/v1/payment.route.js
@@ -44,7 +44,8 @@ router.post("/create-checkout-session", protectRoute, async (req, res) => {
             payment_method_types:["card",],
             line_items: lineItem,
             mode: "payment",
-            success_url:"http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}",
+            success_url:`${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url:`${process.env.CLIENT_URL}/purchase-cancel`,
         })
     }catch (error) {
         console.error(error);

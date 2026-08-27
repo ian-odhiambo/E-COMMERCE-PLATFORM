6400651f01d4bf8e16f2d74edc495315816202cb
ian <odhiamboian096@gmail.com>
2026-08-27 16:15:46 +0300

    another commit

--- BACKEND/src/controllers/coupon.controller.js
+++ BACKEND/src/controllers/coupon.controller.js
@@ -13,8 +13,24 @@ export const getCoupon = async (req, res) => {
 export const validateCoupon = async (req, res) => {
     try{
         const {code} = req.body;
-        const coupon = await Coupon.findOne({code:code, userId:req.user, isActive:true});
-    }catch(error){
+        const coupon = await Coupon.findOne({code:code, userId:req.user._id, isActive:true});
 
+        if(!coupon){
+            return res.status(404).json({ message:"Coupon not found" });
+
+        if(coupon.expirationDate < newDate()){
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(404).json({message: "Coupon expired"});
+        }
+
+        res.json({
+            message: "Coupon is valid",
+            code: coupon.code, 
+            discountPercentage: coupon.discountPercentage});    
+        }
+    }catch(error){
+        console.log("Error in validateCoupon controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message })
     }
 }

884725732aa61264c00c4f32783f090ff81c2a0e
ian <odhiamboian096@gmail.com>
2026-08-28 21:29:34 +0300

    another commit

--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -20,12 +20,15 @@ const SignUpPage = () => {
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
         initial={{ opacity: 0, y:20 }}
         animate={{ opacity: 1, y:0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
         >
         
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400"></h2>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">
+                Create your account
+            </h2>
 
         </motion.div>
     </div>

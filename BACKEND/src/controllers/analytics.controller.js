6fc00ccce08dff16ef18d8c4572c3448f9e9525a
ian <odhiamboian096@gmail.com>
2026-08-28 11:04:52 +0300

    another commit

--- BACKEND/src/controllers/analytics.controller.js
+++ BACKEND/src/controllers/analytics.controller.js
@@ -4,4 +4,22 @@ import Product from '../models/product.model.js'
 export const getAnalyticsData = async (req, res) => {
     const totalUsers = await User.countDocuments();
     const totalProducts = await Product.countDocuments();
+    const salesData = await Order.aggregate([
+        {
+            $group: {
+                _id:null,
+                totalSales: {$sum:1},
+                totalRevenue: {$sum:"$totalAmount"}
+            }
+        }
+    ])
+
+    const {totalSales, totalRevenue} = salesData[0] || {totalSales:0, totalRevenue:0};
+
+    return {
+        users:totalUsers,
+        products:totalProducts,
+        totalSales,
+        totalRevenue
+    }
 }
--- BACKEND/src/routes/v1/analytics.route.js
+++ BACKEND/src/routes/v1/analytics.route.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../../middleware/auth.middleware.js";
+import {getAnalyticsData} from '../../controllers/analytics.controller.js';
 
 const router = express.Router();
 
 router.get("/",protectRoute, adminRoute, async(req, res) => {
     try{
         const analyticsData = await getAnalyticsData();
+
+        const startDate = new Date();
+        const endDate = newDate(endDate.getTime() + 24 * 60 * 60 * 1000); // Add one day to the end date
     }catch(error){
-        
+
     }
 })
 

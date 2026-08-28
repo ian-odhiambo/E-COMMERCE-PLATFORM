dc72fc2d52bb611b8364c59fd986bdcc4598e1e3
ian <odhiamboian096@gmail.com>
2026-08-28 17:12:45 +0300

    another commit

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { ShoppingCart, Userplus, Login, Logout, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
 
-    const 
+    const user = true;
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
         <div className="container mx-auto px-4 py-3">
@@ -12,6 +12,11 @@ const Navbar = () => {
 
             <nav className="flex flex-wrap items-center gap-4">
                 <Link to={"/"} className=''>Home</Link>
+                {user && (
+                    <Link to={"/cart"} className="relative group">
+                        <ShoppingCart className="inline-block mr-1 group-hover:text-emerald-400" size={20}/>
+                    </Link>
+                )}
             </nav>
         </div>
     </header>

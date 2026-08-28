0c31d30d5008ba8ed2947d46db5df6c05369b015
ian <odhiamboian096@gmail.com>
2026-08-28 21:12:04 +0300

    another commit

--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import {useState} from 'react';
+import {Link} from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRght, Loader } from "lucide-react";
+import { motion } from "framer-motion";
 
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } 
 const SignUpPage = () => {
+    const loading = true;
+    const [formData, setFormData] = useState({
+        name:""
+    })
   return (
     <div>SignUpPage</div>
   )

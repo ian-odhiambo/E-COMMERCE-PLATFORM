01fe82b9ea553d616b03437114f28ccdd10eb9a1
ian <odhiamboian096@gmail.com>
2026-08-28 16:17:49 +0300

    another commit

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App;

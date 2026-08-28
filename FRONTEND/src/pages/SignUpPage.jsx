f83ce34d3fadb9610b8469f8d9d46ee53da7779a
ian <odhiamboian096@gmail.com>
2026-08-28 21:27:19 +0300

    another commit

--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -12,9 +12,23 @@ const SignUpPage = () => {
         confirmPassword: "",
     });
 
-    const handleSu
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
   return (
-    <div>SignUpPage</div>
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <motion.div
+        initial={{ opacity: 0, y:20 }}
+        animate={{ opacity: 1, y:0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        >
+        
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400"></h2>
+
+        </motion.div>
+    </div>
   )
 }
 

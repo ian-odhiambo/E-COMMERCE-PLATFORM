0bf6552b0f4182b679a0abfde55887f083236242
ian <odhiamboian096@gmail.com>
2026-08-28 22:33:34 +0300

    another commit

--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -64,7 +64,7 @@ const SignUpPage = () => {
                         </label>
                         <div className="mt-1 relative rounded-md shadow-sm">
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                <User className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                                <Mail className="h-5 w-5 text-gray-400" aria-hidden="true"/>
                             </div>
                             <input
                                 id='email'
@@ -74,6 +74,26 @@ const SignUpPage = () => {
                                 onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                                 className='block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
                                 placeholder='you@example.com'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor='password' className="block text-sm font-medium text-gray-300">
+                            Password
+                        </label>
+                        <div className="mt-1 relative rounded-md shadow-sm">
+                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <Lock className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                            </div>
+                            <input
+                                id='password'
+                                type='password'
+                                required
+                                value={ formData.password }
+                                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                                className='block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                                placeholder='*********'
                             />
                         </div>
                     </div>

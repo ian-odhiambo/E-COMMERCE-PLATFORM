a00a8b7ce7b121f9884e07b86c98ce92fe912889
ian <odhiamboian096@gmail.com>
2026-08-28 10:48:47 +0300

    another commit

--- BACKEND/src/controllers/analytics.controller.js
+++ BACKEND/src/controllers/analytics.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
+import Product from '../models/product.model.js'
 
 export const getAnalyticsData = async (req, res) => {
     const totalUsers = await User.countDocuments();
+    const totalProducts = await Product.countDocuments();
 }

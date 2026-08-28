cde6a1ee12883f04081e89eb2ae7ef5961b9b942
ian <odhiamboian096@gmail.com>
2026-08-28 22:25:05 +0300

    another commit

--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import {Link} from "react-router-dom";
-import { UserPlus, Mail, Lock, User, ArrowRght, Loader } from "lucide-react";
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
 
 const SignUpPage = () => {
@@ -57,6 +57,27 @@ const SignUpPage = () => {
                             />
                         </div>
                     </div>
+
+                    <div>
+                        <label htmlFor='email' className="block text-sm font-medium text-gray-300">
+                            Email address
+                        </label>
+                        <div className="mt-1 relative rounded-md shadow-sm">
+                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <User className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                            </div>
+                            <input
+                                id='email'
+                                type='email'
+                                required
+                                value={ formData.email }
+                                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                                className='block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                                placeholder='you@example.com'
+                            />
+                        </div>
+                    </div>
+
                 </form>
             </div>
         </motion.div>

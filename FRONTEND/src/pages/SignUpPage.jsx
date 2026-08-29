4dcdfa232129b61e70a20773babb035ee4c0840d
ian <odhiamboian096@gmail.com>
2026-08-29 13:51:30 +0300

    another commit

--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -4,7 +4,7 @@ import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
 
 const SignUpPage = () => {
-    const loading = false,
+    const loading = false;
     const [formData, setFormData] = useState({
         name: "",
         email: "",

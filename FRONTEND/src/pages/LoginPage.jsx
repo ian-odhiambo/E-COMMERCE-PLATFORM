c84291e45ba04c5f0dcea8b07d6f76ab046bea1b
ian <odhiamboian096@gmail.com>
2026-08-29 21:18:15 +0300

    anothercommit

--- FRONTEND/src/pages/LoginPage.jsx
+++ FRONTEND/src/pages/LoginPage.jsx
@@ -8,12 +8,13 @@ import { useUserStore } from "../stores/useUserStore";
 const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const loading = true;
-    const { login } = useUserStore();
+
+    const { login, loading } = useUserStore();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email, password);
+        login({ email, password });
     }
 
     return (

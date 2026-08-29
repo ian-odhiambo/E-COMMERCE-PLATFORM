b700f657eabfa15d2ea2b39750c448e9d79a753c
ian <odhiamboian096@gmail.com>
2026-08-29 17:38:52 +0300

    anothercommit

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -4,6 +4,7 @@ import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 
 import Navbar from './components/Navbar';
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
           <Route path='/login' element={<LoginPage/>} />
         </Routes>
       </div>
-
+      <Toaster/>
     </div>
   )
 }

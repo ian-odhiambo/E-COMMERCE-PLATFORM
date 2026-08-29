c3920cd29038414ab457b6aff6e15d0f6e74df39
ian <odhiamboian096@gmail.com>
2026-08-29 12:14:52 +0300

    another commit

--- FRONTEND/src/pages/LoginPage.jsx
+++ FRONTEND/src/pages/LoginPage.jsx
@@ -71,7 +71,7 @@ const LoginPage = () => {
                                 type='password'
                                 required
                                 value={ formData.password }
-                                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                                onChange={(e) => setPassword(e.target.value)}
                                 className='block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
                                 placeholder='*********'
                             />

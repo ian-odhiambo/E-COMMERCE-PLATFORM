45983ae73d513de9af51b08a4e68706311ab20f9
ian <odhiamboian096@gmail.com>
2026-08-29 18:09:23 +0300

    anothercommit

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,16 +1,32 @@
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/v1": {
+//         target: "http://localhost:5000",
+//       },
+//     },
+//   },
+// })
+
+// //http://localhost:5000/api/v1/auth/signup
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1'), // Add this line
       },
     },
   },
 })
-
-//http://localhost:5000/api/v1/auth/signup

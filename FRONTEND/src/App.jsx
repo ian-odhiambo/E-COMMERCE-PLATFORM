7fa00c6bc23f2224c22f55054f280cf694e1a49f
ian <odhiamboian096@gmail.com>
2026-08-30 13:17:23 +0300

    another commit

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -10,11 +10,11 @@ import { useEffect } from 'react';
 
 function App() {
 
-  const { user } = useUserStore();
+  const { user, checkAuth } = useUserStore();
 
   useEffect(() => {
     checkAuth()
-  }, [checkAuth])
+  }, [checkAuth]);
 
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
@@ -30,8 +30,8 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<HomePage/>} />
-          <Route path='/signup' element={<SignUpPage/>} />
-          <Route path='/login' element={user ? <HomePage/> :<LoginPage/>} />
+          <Route path='/signup' element={ !user ? <SignUpPage/> : <Navigate to="/"/>} />
+          <Route path='/login' element={user ? <LoginPage/> :<Navigate to="/"/>} />
         </Routes>
       </div>
       <Toaster/>
